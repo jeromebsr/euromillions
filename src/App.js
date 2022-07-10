1bffde25246a284183cc3347643dd0a2f273a348
Yorinis <jerome.bsrpro@gmail.com>
2022-07-10 18:41:02 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { useState } from "react";
 function App() {
   const [gridInt, setGridInt] = useState([]);
   const [grids, setGrids] = useState([]);
+  const [isActive, setIsActive] = useState(false);
 
   const range = (start, end) => {
     return Array(end - start + 1).fill().map((_, idx) => start + idx)
   }
 
   const checkInt = (i) => {
+    let elem = document.getElementById(i);
+
     if(gridInt.length <= 4) {
       if(gridInt.includes(i)) {
+        elem.classList.remove('grid-btn-active');
         return null;
       }
       setGridInt([...gridInt, i]);
+      elem.classList.add('grid-btn-active');
     }
   }
 
@@ -41,15 +46,22 @@ function App() {
       <div className="grid-game">
         {result.map((i) => (
           <>
-          <label>{i}</label>
-          <input
-            type="radio" 
+          <button
             id={i} 
+            className='grid-btn'
             value={i} 
             onClick={() => checkInt(i)}
-          />
+            // style={{ 
+            //   backgroundColor: isActive ? '#001367' : null, 
+            //   color: isActive ? 'white' : null 
+            // }}
+          >
+            {i}
+          </button>
           </>
         ))}
+      </div>
+      <div>
         <button onClick={() => clearGrid()}>Effacer</button>
         <button onClick={() => confGrid()}>Valider</button>
       </div>
