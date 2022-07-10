4f99e9873539f4845cb60e03cc7610d779d9a430
Jérôme Boursier <yorinis@mbp-de-jerome.home>
2022-07-10 17:43:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [gridInt, setGridInt] = useState([]);
+  const [grids, setGrids] = useState([]);
+
+  const range = (start, end) => {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+  }
+
+  const checkInt = (i) => {
+    if(gridInt.length <= 4) {
+      if(gridInt.includes(i)) {
+        return null;
+      }
+      setGridInt([...gridInt, i]);
+    }
+  }
+
+  const clearGrid = () => {
+    setGridInt([]);
+  }
+
+  const confGrid = () => {
+    if(gridInt.length === 5) {
+      setGrids([{...grids, gridInt}]);
+    }else {
+      console.log("Erreur: Il faut 5 chiffres.")
+    }
+    
+  }
+
+  let result = range(1, 50);
+
+  console.log(gridInt.length);
+  console.log(gridInt.sort());
+  console.log(grids);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="grid-game">
+        {result.map((i) => (
+          <>
+          <label>{i}</label>
+          <input
+            type="radio" 
+            id={i} 
+            value={i} 
+            onClick={() => checkInt(i)}
+          />
+          </>
+        ))}
+        <button onClick={() => clearGrid()}>Effacer</button>
+        <button onClick={() => confGrid()}>Valider</button>
+      </div>
+      <div className="resume">
+          <h2>Mes grilles</h2>
+          {grids.map((i) => (
+            i.gridInt
+          ))}
+      </div>
     </div>
   );
 }
